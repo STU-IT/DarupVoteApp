d26a7adab8c99dda82798d5df62b3c7a879e1b93
Henrik <ny123mail321@gmail.com>
2017-02-08 09:39:41 +0100

    radio buttons

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,71 @@
-/**
- * Created by smag on 07-02-2017.
- */
+var questionId;
+
+function prepareStem(event, ui)
+{
+
+    $.get
+    (
+        'http://grahn.dk/darup/vote.php?qid=' + questionId,
+        function(res, code)
+        {
+            //console.debug(code + ": " + JSON.stringify(res));
+
+            // check lige om indholdes er bare lidt i orden...
+            if ( res[questionId].answers)
+            {
+                // fjern 'gamle' div'er
+                $('#stem label').remove();
+                var newContent = '';
+                // gennemløb alle afsnit i Items
+
+                // res[questionID].question
+
+                for (var i in res[questionId].answers)
+                {
+                    // lav en ny "div" for hver sæson
+                    // <div>
+                    //     <a href="#episodeDetails">
+                    //         <img src="xxx">
+                    //         <h2>2 3-10</h2>
+                    //     <h2>Jonas kysser Eva, men Noora ser det hele</h2>
+                    //     </a>
+                    //     </div>
+                    newContent +=
+                        '<div><label>' +
+                        '<input type="radio" name="radio-choice-0" id=' + i +' class="custom">' + res[questionId].answers[i].answer +
+                        '</label></div>';
+                }
+                $(newContent).appendTo("#stem")
+            }
+        }
+    )
+}
+
+$(document).ready( // når siden er loaded
+    function()
+    {
+        var pageContainer = $("body").pagecontainer
+        (
+            {
+            beforeshow:
+                function( event, ui)
+                {
+                    //hvilken side er vi ved at vise
+                    console.log("beforeshow: " + ui.toPage[0].id);
+
+                    switch(ui.toPage[0].id)
+                    {
+                        case "stem":
+                            // debug mockUp
+                            questionId = 2;
+                            prepareStem(event, ui);
+                            break;
+
+                        case "landingpage":
+                            break;
+                    }
+                }
+            }
+        )
+    }
+);
